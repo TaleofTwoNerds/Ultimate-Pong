5dfce7088bc73ceac4684c343c42851a50a6a327
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-01-21 09:01:18 -0600

    Bug Fixes
    Fixed bugs within the SinglePlayer.java file. Increased boundaries for
    the bats and added in variables to allow scoring and hit values. Added
    a velocity variable for the bats. Added score into the display title.

--- src/com/totn/mainGame/SinglePlayer.java
+++ src/com/totn/mainGame/SinglePlayer.java
@@ -5,6 +5,7 @@ import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 import com.totn.entities.AbstractMoveableEntity;
 
@@ -14,12 +15,18 @@ public class SinglePlayer {
 	
     private static final int width = 640;
     private static final int height = 480;
+    private static int pIs = 0;
+    private static int hit = 0;
+    private static int pIIs = 0;
     private static boolean isRunning = true;
     private static Ball ball;
     private static Bat bat;
     private static Bat opbat;
-    public static double velocity = .25;
-    public static double yvelocity = velocity / 2;
+    private static double velocity = .25;
+    private static double yvelocity = velocity / 2;
+    private static final double pSpeed = .25;
+    private static double pISpeed = .25;
+    private static double pIISpeed = .25;
 
     public SinglePlayer() 
     {
@@ -49,20 +56,20 @@ public class SinglePlayer {
 //    	Move the bat up or down depending on the W/S keys
         if (bat.getY() < height - 90 && Keyboard.isKeyDown(Keyboard.KEY_W) ) 
         {
-                bat.setDY(.2);
+                bat.setDY(pISpeed);
         	} else if (bat.getY() > 0 + 10 && Keyboard.isKeyDown(Keyboard.KEY_S)) 
         	{
-                bat.setDY(-.2);
+                bat.setDY(-pISpeed);
         	} else {
                 bat.setDY(0);
         	}
 //      Move the bat up or down depending on the Up/Down arrows
         if (opbat.getY() < height - 90 && Keyboard.isKeyDown(Keyboard.KEY_UP) ) 
         {
-        		opbat.setDY(.2);
+        		opbat.setDY(pIISpeed);
         	} else if (opbat.getY() > 0 + 10 && Keyboard.isKeyDown(Keyboard.KEY_DOWN)) 
         	{
-        		opbat.setDY(-.2);
+        		opbat.setDY(-pIISpeed);
         	} else {
         		opbat.setDY(0);
         	}
@@ -77,8 +84,13 @@ public class SinglePlayer {
         if(Keyboard.isKeyDown(Keyboard.KEY_T))
         {
             ball.setDX(velocity);
-            yvelocity = Math.random(); 
-            ball.setDY(-yvelocity);
+            ball.setDY(yvelocity);
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_Q))
+        {
+        	pIISpeed = .1;
+        } else {
+        	pIISpeed = pSpeed; 
         }
     }
 
@@ -103,7 +115,6 @@ public class SinglePlayer {
 //    	Starts the display
         try {
             Display.setDisplayMode(new DisplayMode(width, height));
-            Display.setTitle("Pong");
             Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
@@ -141,40 +152,93 @@ public class SinglePlayer {
         bat.draw();
         opbat.draw();
     }
-
+    
     private static void logic(int delta) 
     {
         ball.update(delta);
         bat.update(delta);
         opbat.update(delta);
+        
+        
+//        Increases the speed of the ball as the amount of hits increases
+        if(hit==0)
+        {
+        	velocity = .3;
+        } else {
+            velocity = .3 + (hit + 1) * .01;
+        }
+        
+//        Sets the display title before every tick
+        Display.setTitle("Pong" + " | " + "P1: " + pIs + " | " + "P2: " + pIIs + " | " + "Hit: " + hit);
+        
 //        If ball hits bat
-        if (ball.getX() <= bat.getX() + bat.getWidth() && ball.getX() >= bat.getX() && ball.getY() >= bat.getY() &&
-                ball.getY() <= bat.getY() + bat.getHeight()) 
+        if (ball.getX() <= bat.getX() + bat.getWidth() && ball.getX() >= bat.getX()/* && ball.getY() >= bat.getY() - ball.getHeight() &&
+                ball.getY() <= bat.getY() + bat.getHeight()*/) 
         {
             ball.setDX(velocity);
+            ball.setDY(yvelocity);
+            hit++;
         }
 //        If ball hits opbat
-        if (ball.getX() <= opbat.getX() && ball.getX() >= opbat.getX() - opbat.getWidth() && ball.getY() >= opbat.getY() &&
+        if (ball.getX() <= opbat.getX() && ball.getX() >= opbat.getX() - opbat.getWidth() && ball.getY() >= opbat.getY() - ball.getHeight() &&
                 ball.getY() <= opbat.getY() + opbat.getHeight())
         {
-            ball.setDX(- velocity);
+            ball.setDX(-velocity);
+            ball.setDY(yvelocity);
+            hit++;
         }
 //        If the ball bounces of the top or the bottom
         if (ball.getY() >= height - ball.getHeight())
         {
-        	ball.setDY(- yvelocity);
-        } else if (ball.getY() <= 0) { 
+        	yvelocity = -yvelocity;
+        	ball.setDY(yvelocity);
+        } else if (ball.getY() <= 0) 
+        { 
+        	yvelocity = -yvelocity;
         	ball.setDY(yvelocity);
         }
-//        For when the ball goes off the page
-        if (ball.getX() > width || ball.getX() < 0)
+//        For when the ball scores
+        if(ball.getX() > width)
         {
-        	ball.setX(width / 2 - 10 / 2);
-        	ball.setY(height / 2 - 10 / 2);
+       		if(opbat.getY() < height / 2)
+       		{
+       			yvelocity = 0 - Math.abs(yvelocity);
+       			ball.setDY(yvelocity);
+       		} else 
+       		{
+       			yvelocity = Math.abs(yvelocity);
+       			ball.setDY(yvelocity);
+       		}
+       		
+        	ball.setDX(-Math.abs(ball.getDX()));
+            ball.setX(width / 2 + 150);
+            ball.setY(height / 2 - 10 / 2);
+
+            pIs++;
+            hit = -1;
+            hit++;
+        } else if (ball.getX() < 0)
+        {
+        	if(bat.getY() < height / 2)
+        	{
+        		yvelocity = 0 - Math.abs(yvelocity);
+        		ball.setDY(yvelocity);
+        	} else 
+        	{
+        		yvelocity = Math.abs(yvelocity);
+        		ball.setDY(yvelocity);
+        	}
+        	ball.setDX(Math.abs(ball.getDX()));
+            ball.setX(width / 2 - 150);
+            ball.setY(height / 2 - 10 / 2);
+            
+            pIIs++;
+            hit = -1;
+            hit++;
         }
     }
-
-    private static class Bat extends AbstractMoveableEntity 
+//    Creates the bat and ball classes
+    public static class Bat extends AbstractMoveableEntity 
     {
 
         public Bat(double x, double y, double width, double height) 
@@ -189,9 +253,8 @@ public class SinglePlayer {
         }
     }
 
-    private static class Ball extends AbstractMoveableEntity 
+    public static class Ball extends AbstractMoveableEntity 
     {
-
         public Ball(double x, double y, double width, double height) 
         {
             super(x, y, width, height);
